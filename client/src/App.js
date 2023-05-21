806230fba73c93a86504b94c36bc879af6671996
Kettengeschwindigkeit <666damned999@mail.ru>
2023-05-21 12:43:00 +0300

    Pages routes settings [front-end]

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { useRoutes } from './routes'
 import 'materialize-css'
 
 function App() {
+  const routes = useRoutes(true)
+
   return (
-    <div className='container'>
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className='container'>
+        {routes}
+      </div>
+    </Router>
   )
 }
 
