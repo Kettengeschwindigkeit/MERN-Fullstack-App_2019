f390a92d0f71a03659667766aecca274dcd9b79b
Kettengeschwindigkeit <666damned999@mail.ru>
2023-05-23 21:56:06 +0300

    AuthPage component

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AuthPage = () => {
+  const [form, setForm] = useState({ email: '', password: '' })
+
+  const changeHandler = event => {
+    setForm({ ...form, [event.target.name]: event.target.value })
+  }
+
   return (
-    <div>
-      <h1>Auth Page</h1>
+    <div className='row'>
+      <div className='col s6 offset-s3'>
+        <h1>Cut the Link</h1>
+        <div className='card blue darken-1'>
+          <div className='card-content white-text'>
+            <span className='card-title'>Authorization</span>
+            <div>
+              <div className='input-field'>
+                <input className='yellow-input' type='text' name='email' id='email' placeholder='Enter email' onChange={changeHandler} />
+                <label htmlFor='email'>Email</label>
+              </div>
+              <div className='input-field'>
+                <input className='yellow-input' type='password' name='password' id='password' placeholder='Enter the password' onChange={changeHandler} />
+                <label htmlFor='password'>Password</label>
+              </div>
+            </div>
+          </div>
+          <div className='card-action'>
+            <div className='card-action'>
+              <button className='btn yellow darken-4' style={{ marginRight: 10 }}>Sign In</button>
+              <button className='btn grey lighten-1' black-text>Registration</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
